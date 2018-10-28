b92ba1b9fe1f65100f4a205934f88a5ab600120c
tanakorn128 <60030041@kmitl.ac.th>
2018-10-28 17:00:20 +0700

    ok

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Thanakorn", "60030041");
+            
+
+            Console.WriteLine("Student name = " + s1.Name + ", ID = " + s1.Id );
+            Console.ReadLine();
+        }
+        public class Student
+        {
+            private string name;
+            private string id;
+            private float gpa;
+
+            public Student(string value1, string value2)
+            {
+                this.name = value1;
+                this.id = value2;
+                
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+            public string Id
+            {
+                get { return id; }
+            }
+            public float Gpa
+            {
+                get { return gpa; }
+                set { gpa = value; }
+            }
+
+        }
+        
+
+    }
+}

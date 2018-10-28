ec720ef001ccf104590dbed944abdb413284a218
tanakorn128 <60030041@kmitl.ac.th>
2018-10-28 16:03:59 +0700

    ok

---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public class Student
+        {
+            private string name;
+            private string id;
+            private float gpa;
+
+            public string Name
+            {
+                get { return name; }
+            }
+            public string Id
+            {
+                get { return id; }
+            }
+            public float Gpa
+            {
+                get { return gpa; }
+                set { gpa = value; }
+            }
+
+        }
+    }
+}

52814301a69430fffbe0c197eed3085f7ed8686e
tanakorn128 <60030041@kmitl.ac.th>
2018-11-02 09:40:20 +0700

    ok

---
+++ object/object/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point myPoint = new Point();  // create new object
+            myPoint.X = 10;
+            myPoint.Y = 10;
+            Console.WriteLine("myPoint.x = {0}", myPoint.X);
+            Console.WriteLine("myPoint.y = {0}", myPoint.Y);
+            Console.ReadLine();
+
+        }
+        class Point          // class name
+        {
+            private int x;    // fields
+            private int y;
+
+            public int X      // properties
+            {
+                get { return x; }
+                set { x = value; }
+            }
+
+            public int Y
+            {
+                get { return y; }
+                set { y = value; }
+            }
+        }
+
+    }
+}
---
+++ การปกปิดข้อมูลหรือการห่อหุ้ม (Encapsulation)/การปกปิดข้อมูลหรือการห่อหุ้ม (Encapsulation)/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace การปกปิดข้อมูลหรือการห่อหุ้ม__Encapsulation_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student su = new Student("Student Name", "12345678");
+            try
+            {
+                // su.Name = "My name";
+                su.GPA = 4.0f;
+                Console.WriteLine("Student name : " + su.Name);
+                Console.WriteLine("Student ID   : " + su.ID);
+                Console.WriteLine("Student GPA  : " + su.GPA);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Student
+    {
+        private string name;
+        private string id;
+        private float gpa;
+
+        public Student(string Name, string ID)
+        {
+            this.name = Name;   // assign auto variable to the field
+            this.id = ID;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public string ID
+        {
+            get { return id; }
+        }
+        public float GPA
+        {
+            get
+            {
+                return gpa;
+            }
+            set
+            {
+                if (value > 0.0 && value <= 4.0)
+                    gpa = value;
+                else
+                    throw (new Exception("Error!!!! invalid GPA"));
+            }
+        }
+    }
+}
+
